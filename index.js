2a485f7c187788ca1888ce062c56d920e45f1d3f
Alex <suslo2000@list.ru>
2025-08-28 22:07:38 +0300

    Added function that creates Html card and fills it with information about the book from the object Book

--- index.js
+++ index.js
@@ -26,23 +26,52 @@ const library = [
 ]
 
 function Book(title, author, year, amountOfPages, isRead, id) {
-	this.id = id;
-	this.title = title;
-	this.author = author;
-	this.year = year;
-	this.amountOfPages = amountOfPages;
-	this.isRead = isRead;
+  this.id = id
+  this.title = title
+  this.author = author
+  this.year = year
+  this.amountOfPages = amountOfPages
+  this.isRead = isRead
 }
 
+const bookCardsDiv = document.querySelector('.book-cards')
 
 function addBookToLibrary(title, author, year, amountOfPages, isRead) {
-	const booksId = crypto.randomUUID();
-	const newBook = new Book(title, author, year, amountOfPages, isRead, booksId);
-	library.push(newBook)
+  const booksId = crypto.randomUUID()
+  const newBook = new Book(title, author, year, amountOfPages, isRead, booksId)
+  library.push(newBook);
+
+  displayBookInLibrary(title, author, year, amountOfPages)
+}
+
+function displayBookInLibrary(title, author, year, amountOfPages) {
+  const bookCardDiv = document.createElement('div')
+  bookCardDiv.classList.add('book-card')
+  bookCardsDiv.appendChild(bookCardDiv)
+
+  const authorPara = document.createElement('p')
+  authorPara.classList.add('author')
+  authorPara.innerHTML = author
+  bookCardDiv.appendChild(authorPara)
+
+  const titlePara = document.createElement('p')
+  titlePara.classList.add('title')
+  titlePara.innerHTML = title
+  const breakString = document.createElement('br')
+  titlePara.appendChild(breakString)
+  const yearsSpan = document.createElement('span')
+  yearsSpan.innerHTML = `(${year})`
+  titlePara.appendChild(yearsSpan)
+  bookCardDiv.appendChild(titlePara)
+
+  const pagesPara = document.createElement('p')
+  pagesPara.classList.add('pages')
+  pagesPara.innerHTML = amountOfPages
+  bookCardDiv.appendChild(pagesPara)
 }
 
 function logBooks() {
-	for (let i = 0; i < library.length; i++) {
-		console.log(library[i])
-	}
+  for (let i = 0; i < library.length; i++) {
+    console.log(library[i])
+  }
 }
